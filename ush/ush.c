a70948c06a77eeaa9b9901ea3b04bc26ce58a4bc
Jamal Marri <marrij@wwu.edu>
2020-04-10 11:00:46 -0700

    Implement argument counting with double quotes.

--- ush/ush.c
+++ ush/ush.c
@@ -35,25 +35,25 @@ main (void)
 
     while (1) {
 
-        /* prompt and get line */
-	fprintf (stderr, "%% ");
-	if (fgets (buffer, LINELEN, stdin) != buffer)
-	  break;
+    /* prompt and get line */
+    fprintf (stderr, "%% ");
+    if (fgets (buffer, LINELEN, stdin) != buffer)
+        break;
 
-        /* Get rid of \n at end of buffer. */
-	len = strlen(buffer);
-	if (buffer[len-1] == '\n')
-	    buffer[len-1] = 0;
+    /* Get rid of \n at end of buffer. */
+    len = strlen(buffer);
+    if (buffer[len-1] == '\n')
+        buffer[len-1] = 0;
 
-	/* Run it ... */
-	processline (buffer);
+    /* Run it ... */
+    processline (buffer);
 
     }
 
     if (!feof(stdin))
         perror ("read");
 
-    return 0;		/* Also known as exit (0); */
+    return 0;       /* Also known as exit (0); */
 }
 
 
@@ -63,30 +63,33 @@ void processline (char *line)
     int    status;
     int argc; // Number of arguments for executed program
     char **argpointers = arg_parse(line, &argc);
+
+    printf("%d\n", argc);
+
     // Only attempt to execute if input contained any arguments
     if (argc > 0) {
         /* Start a new process to do the job. */
         cpid = fork();
         if (cpid < 0) {
-          /* Fork wasn't successful */
-          perror ("fork");
-          return;
+            /* Fork wasn't successful */
+            perror ("fork");
+            return;
         }
 
         /* Check for who we are! */
         if (cpid == 0) {
-          /* We are the child! */
-          execvp(argpointers[0], argpointers);
-          /* execlp returned, wasn't successful */
-          perror ("exec");
-          fclose(stdin);  // avoid a linux stdio bug
-          exit (127);
+            /* We are the child! */
+            execvp(argpointers[0], argpointers);
+            /* execlp returned, wasn't successful */
+            perror ("exec");
+            fclose(stdin);  // avoid a linux stdio bug
+            exit (127);
         }
 
         /* Have the parent wait for child to complete */
         if (wait (&status) < 0) {
-          /* Wait wasn't successful */
-          perror ("wait");
+            /* Wait wasn't successful */
+            perror ("wait");
         }
     }
     free(argpointers);
@@ -108,6 +111,18 @@ char ** arg_parse (char *line, int *argcptr)
             argc++;
             // Find end of argument
             while (line[ptr] != ' ' && line[ptr] != 0) {
+                // If quote is found, find its partner
+                if (line[ptr] == '"') {
+                    ptr++;
+                    while (line[ptr] != '"' && line[ptr] != 0) {
+                        ptr++;
+                    }
+                    // Print error and return if EOS is reached before another quote is found
+                    if (line[ptr] == 0) {
+                        fprintf(stderror, "Odd number of quotes found in input.\n");
+                        return NULL;
+                    }
+                }
                 ptr++;
             }
         }
