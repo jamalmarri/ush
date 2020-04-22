e0d7a422101fabbadba2213daa629a2c336a52fc
Jamal Marri <marrij@wwu.edu>
2020-04-22 13:33:16 -0700

    Switch to an array of structs in builtin. Absorb run_builtin function.

--- ush/builtin.c
+++ ush/builtin.c
@@ -1,31 +1,44 @@
+#include <string.h>
+
 #include "defn.h"
 
 // Prototypes
-void exit_shell(char **argpointers);
-void envset(char **argpointers);
-void envunset(char **argpointers);
-void cd(char **argpointers);
+void exit_shell(char **argpointers, int argc);
+void envset(char **argpointers, int argc);
+void envunset(char **argpointers, int argc);
+void cd(char **argpointers, int argc);
 
 typedef void (*funcptr)(char **);
 
-static funcptr builtins[] = {exit_shell,envset,envunset,cd};
+struct builtin {
+    char *name;
+    funcptr function;
+};
 
-int check_for_builtin(char *argpointer, int argc) {
+static builtin builtins[] = {{"exit", exit_shell},
+                             {"envset", envset},
+                             {"envunset", envunset},
+                             {"cd", cd}};
+
+int check_for_builtin(char **argpointers, int argc) {
+    int num_builtins = (int) (sizeof(builtins) / sizeof(builtins[0]));
+    for (int i = 0; i < num_builtins; i++) {
+        if (!strcmp(argpointers[0], builtins[i].name)) {
+            (*builtins[i].function)(argpointers, argc);
+            return 1;
+        }
+    }
     return 0;
 }
 
-void run_builtin(int builtin, char **argpointers) {
-    (*builtins[builtin])(argpointers);
+void exit_shell(char **argpointers, int argc) {
 }
 
-void exit_shell(char **argpointers) {
+void envset(char **argpointers, int argc){
 }
 
-void envset(char **argpointers){
+void envunset(char **argpointers, int argc) {
 }
 
-void envunset(char **argpointers) {
-}
-
-void cd(char **argpointers) {
+void cd(char **argpointers, int argc) {
 }
