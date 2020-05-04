7ef4fddeb9c4d1ce2f71d2ef2bb610484dd6ec1c
Jamal Marri <marrij@wwu.edu>
2020-05-03 17:54:27 -0700

    Correct implementation of global variables in defn header.

--- ush/defn.h
+++ ush/defn.h
@@ -3,3 +3,9 @@
 int expand(char *orig, char *new, int newsize);
 
 int check_for_builtin(char **argpointers, int argc);
+
+/* Global Variables */
+
+int mainargc;
+char **mainargv;
+int shift_offset;
